9d11bf4d40574fd3d2e326279182f882ff9094c0
hyunwoo <bhw9102@gmail.com>
2019-01-24 13:25:04 +0900

    관리자 페이지에 보이게 하다.

--- session/admin.py
+++ session/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from session.models import Session, Construction
 
-# Register your models here.
+
+class SessionAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at', )
+    list_display_links = ('id', 'title', )
+
+
+class ConstructionAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'card', 'position', 'order', )
+    list_display_links = ('id', )
+
+
+admin.site.register(Session, SessionAdmin)
+admin.site.register(Construction, ConstructionAdmin)
