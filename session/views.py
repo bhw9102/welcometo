4fc9d499b1a12b3094203b6d3c4347cd680796f6
hyunwoo <bhw9102@gmail.com>
2019-01-25 17:12:43 +0900

    html의 오류를 수정하다.

--- session/views.py
+++ session/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from session.models import Session, Construction
+from session.models import Session, Construction, print_deck_list
 from session import position
 
 
@@ -18,6 +18,7 @@ def turn_constructions(request, session_id, turn):
 
 
 def open_constructions(session, turn):
+    # print_deck_list(session=session)
     # 턴에 해당하는 카드를 찾는다.
     left_num = Construction.objects.filter(session=session, order=turn, position=position.LEFT).first()
     center_num = Construction.objects.filter(session=session, order=turn, position=position.CENTER).first()
@@ -26,11 +27,11 @@ def open_constructions(session, turn):
     # 이펙트 효과를 가진 카드는 이전 턴 카드다.
     effect_order = turn - 1
     left_effect = Construction.objects.filter(session=session, order=effect_order, position=position.LEFT).first()
-    center_effect = Construction.objects.filter(session=session, order=effect_order,
-                                                position=position.CENTER).first()
+    center_effect = Construction.objects.filter(session=session, order=effect_order, position=position.CENTER).first()
     right_effect = Construction.objects.filter(session=session, order=effect_order, position=position.RIGHT).first()
     open_cards = dict(turn=turn, left_num=left_num, center_num=center_num, right_num=right_num,
                       left_effect=left_effect, center_effect=center_effect, right_effect=right_effect)
+    # print(open_cards)
     return open_cards
 
 
