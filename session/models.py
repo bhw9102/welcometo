d7f91ec82fc2fb9f1be1d3b5671d7acb2373a404
hyunwoo <bhw9102@gmail.com>
2019-01-25 09:56:41 +0900

    게임 덱을 준비하다.

--- session/models.py
+++ session/models.py
@@ -1,5 +1,6 @@
 import datetime
 from django.db import models
+import random
 from game.models import ConstructionClass
 
 
@@ -24,29 +25,72 @@ class Session(CreatedUpdatedMixin, models.Model):
     def prepare_session(self):
         # TODO: 이미 생성되어있을 경우 작동하지 않도록 한다.
         # 카드를 생성한다.
-        prepare_session_card(self)
+        deck = prepare_creating_card(self)
         # 전체를 섞는다.
+        prepare_shuffling_card(deck)
         # 세개 덱으로 나눈다.
-        # 덱 별로 섞는다.
+        prepare_dividing_deck(deck=deck)
+        # 덱이 잘 생성됐는지 확인한다.
+        print_deck_list(session=self)
 
 
-def prepare_session_card(session: Session):
+def prepare_creating_card(session: Session):
     card_list = ConstructionClass.objects.all()
     prepare_position = 0
     order = 0
     prepare_deck = list()
-    for card in card_list:
-        for i in range(0, card.count):
-            card = Construction.create(session=session, card=card, position=prepare_position, order=order)
+    for card_class in card_list:
+        for i in range(0, card_class.count):
+            card = Construction.create(session=session, card=card_class, position=prepare_position, order=order)
             prepare_deck.append(card)
             order += 1
+    return prepare_deck
 
+
+def prepare_shuffling_card(deck):
+    order_list = shuffle_ordering(count=len(deck))
+    for i, obj in enumerate(deck):
+        obj.order = order_list[i]
+        obj.save()
+
+
+def shuffle_ordering(count: int):
+    order_list = list(range(0, count))
+    random.shuffle(order_list)
+    return order_list
+
+
+def prepare_dividing_deck(deck: list):
+    for card in deck:
+        card.position = (card.order % 3) + 1
+        card.order = card.order // 3
+        card.save()
+
+
+def print_deck_list(session: Session):
+    deck = Construction.objects.filter(session=session, position=pos_left).order_by('order').all()
+    print("왼쪽 카드 뭉치")
+    for card in deck:
+        print(card.__str__())
+    deck = Construction.objects.filter(session=session, position=pos_center).order_by('order').all()
+    print("중앙 카드 뭉치")
+    for card in deck:
+        print(card.__str__())
+    deck = Construction.objects.filter(session=session, position=pos_right).order_by('order').all()
+    for card in deck:
+        print(card.__str__())
+
+
+pos_prepare = 0
+pos_left = 1
+pos_center = 2
+pos_right = 3
 
 DECK_POSITION = (
-    ('PREPARE', 0),
-    ('LEFT', 1),
-    ('CENTER', 2),
-    ('RIGHT', 3)
+    ('PREPARE', pos_prepare),
+    ('LEFT', pos_left),
+    ('CENTER', pos_center),
+    ('RIGHT', pos_right)
 )
 
 
